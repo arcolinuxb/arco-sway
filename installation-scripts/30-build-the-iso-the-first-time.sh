969af94cda6d171b7e52abefbc476efebb8a4896
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-02 14:06:29 +0100

    fixing errors

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -26,8 +26,8 @@ echo
 	#Let us set the desktop"
 	#First letter of desktop is small letter
 
-	desktop="hyprland"
-	dmDesktop="hyprland"
+	desktop="sway"
+	dmDesktop="sway"
 
 	arcolinuxVersion='v23.01.01'
 
